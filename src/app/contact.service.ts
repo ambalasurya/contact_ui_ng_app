6d905f26f2569e1bf45370894fa01dca6db42c52
ambalasurya <142885267+ambalasurya@users.noreply.github.com>
2024-06-02 13:28:05 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://3.110.213.226:8081/"
+  private baseUrl="afa1e10fb0f6049c48de621aa1be97a1-414706174.ap-south-1.elb.amazonaws.com"
   
   constructor(private httpClient:HttpClient) { }
 
